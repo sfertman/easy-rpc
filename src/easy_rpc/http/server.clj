dd3c027d3e113de7e6d5fdef28b786f0415c08b7
sfertman <sfertman@zoho.com>
2020-07-26 23:22:21 -0400

    Break down request handler

--- src/easy_rpc/http/server.clj
+++ src/easy_rpc/http/server.clj
@@ -5,27 +5,48 @@
     [org.httpkit.server :as http]
     [reitit.ring :as ring]))
 
+(def wrap-deserialized-body
+  {:name ::wrap-deserialized-body
+   :description "turns bytes to clojure object"
+   :wrap (fn [handler]
+          (fn [request]
+            (-> request
+                :body
+                .bytes
+                util/deserialize
+                handler)))})
+
+(defn response
+  [rpc-handler args]
+  (try
+    {:status 200
+     :body (rpc-handler args)}
+    (catch Throwable e
+      {:status 500
+       :body e})))
+
+(defn serialized-body
+  [response]
+  (update
+    response
+    :body
+    (fn [x] (-> x util/serialize io/input-stream))))
+
+(defn handler
+  [rpc-handler args]
+  (serialized-body (response rpc-handler args)))
+
 (defn api
   [rpc-handler]
-    (ring/ring-handler
-      (ring/router
-        [[ "/"
-          {:post {
-            :handler (fn [{msg :body}]
-              (try
-                {:status 200
-                 :body (-> msg
-                           .bytes
-                           util/deserialize
-                           rpc-handler
-                           util/serialize
-                           io/input-stream)}
-                (catch Throwable e
-                  {:status 500
-                   :body (-> e
-                             util/serialize
-                             io/input-stream )})))}}]])
-      (ring/routes (ring/create-default-handler))))
+  (ring/ring-handler
+    (ring/router [
+      ["/" {
+        :post {
+          :middleware [[wrap-deserialized-body]]
+          :handler (partial handler rpc-handler )}}]])
+          ;; ^ Make this a constant required from rpc-server or something;
+          ;; api doesn't really need to be a function
+    (ring/routes (ring/create-default-handler))))
 
 (defn start
   [server]
--- src/easy_rpc/web/server.clj
+++ src/easy_rpc/web/server.clj
@@ -9,26 +9,47 @@
   [bs]
   (apply str (map char bs)))
 
+(def wrap-deserialized-body
+  {:name ::wrap-deserialized-body
+   :description "turns bytes to clojure object"
+   :wrap (fn [handler]
+          (fn [request]
+            (-> request
+                :body
+                .bytes
+                bytes->str
+                edn/read-string
+                enc/decode-bytes
+                handler)))})
+
+(defn response
+  [rpc-handler args]
+  (try
+    {:status 200
+     :body (rpc-handler args)}
+    (catch Throwable e
+      {:status 500
+       :body e})))
+
+(defn serialized-body
+  [response]
+  (update response :body str))
+
+(defn handler
+  [rpc-handler args]
+  (serialized-body (response rpc-handler args)))
+
 (defn api
   [rpc-handler]
-    (ring/ring-handler
-      (ring/router
-        [ ["/"
-            {:post {
-             :handler (fn [{msg :body}]
-               (try
-                 {:status 200
-                  :body (-> msg
-                            .bytes
-                            bytes->str
-                            edn/read-string
-                            enc/decode-bytes
-                            rpc-handler
-                            str)}
-                 (catch Throwable e
-                   {:status 500
-                    :body (-> e str)})))}}]])
-      (ring/routes (ring/create-default-handler))))
+  (ring/ring-handler
+    (ring/router [
+      ["/" {
+        :post {
+          :middleware [[wrap-deserialized-body]]
+          :handler (partial handler rpc-handler )}}]])
+          ;; ^ Make this a constant required from rpc-server or something;
+          ;; api doesn't really need to be a function
+    (ring/routes (ring/create-default-handler))))
 
 (defn start
   [server]
