1045f1969240a0303e182e36f2cc571998f27fa9
sfertman <sfertman@zoho.com>
2020-07-28 21:30:41 -0400

    bump clojure version

--- project.clj
+++ project.clj
@@ -5,7 +5,7 @@
     :url "https://opensource.org/licenses/mit-license.php"}
   :dependencies [
     [http-kit "2.3.0"]
-    [org.clojure/clojure "1.9.0"]
+    [org.clojure/clojure "1.10.0"]
     [com.taoensso/nippy "2.14.0"]
     [metosin/reitit-ring "0.3.1"]]
   :plugins [
