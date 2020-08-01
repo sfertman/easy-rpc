57129eb946bd8b8ba7eeefcab6fee2a04bc74157
sfertman <sfertman@zoho.com>
2020-07-31 20:34:07 -0400

    copy http changes to web

--- src/easy_rpc/server/web.clj
+++ src/easy_rpc/server/web.clj
@@ -30,20 +30,22 @@
 (def wrap-serialization
   {:name ::wrap-serialization
    :description "Deserializes request body before handler and serializes response body after handler"
-   :wrap (fn [handler]
+   :wrap (fn [handler [deserialized-body serialized-body]]
           (fn [request]
             (let [response (handler (deserialized-body request))]
               (serialized-body response))))})
 
 (defn api
-  [handler]
-  (ring/ring-handler
-    (ring/router [
-      ["/" {
-        :post {
-          :middleware [[wrap-serialization]]
-          :handler handler}}]])
-    (ring/routes (ring/create-default-handler))))
+  ([handler]
+    (api handler [deserialized-body serialized-body]))
+  ([handler serialization]
+    (ring/ring-handler
+      (ring/router [
+        ["/" {
+          :post {
+            :middleware [[wrap-serialization serialization]]
+            :handler handler}}]])
+      (ring/routes (ring/create-default-handler)))))
 
 (defn handler
   [config request]
@@ -55,9 +57,10 @@
        :body e})))
 
 (defn start!
-  [config]
-  (let [app (api (partial handler config))
+  [config & {:keys [serialization]}]
+  (let [h-fn (partial handler config)
+        app (if serialization (api h-fn serialization) (api h-fn) )
         port (:port config)]
     (let [started-server (http/run-server app {:port port})]
-      (println "easy-rpc web server listening on" port)
+      (println "easy-rpc http server listening on" port)
       started-server)))
